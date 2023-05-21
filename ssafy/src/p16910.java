32229c9e28fcb1f0cd288455c13e1d2280bb89fc
cnsejr2 <cnsejr345@gmail.com>
2023-05-21 11:51:37 +0900

    23.05.21 - p16910

--- ssafy/src/p16910.java
+++ ssafy/src/p16910.java
@@ -8,12 +8,29 @@ public class p16910 {
 		
 		int t = sc.nextInt();
 		
-		for (int i = 0; i < t; i++) {
+		for (int i = 1; i <= t; i++) {
 			int n = sc.nextInt();
-			
-			
+			int result = calculate(n);
+			System.out.println("#" + i + " " + result);
 		}
 		sc.close();
+	}
+
+	private static int calculate(int n) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for (int i = 1; i <= n ; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (Math.pow(i, 2) + Math.pow(j, 2) <= Math.pow(n,  2)) {
+					count++;
+				}
+			}
+		}
+		
+		count *= 4;
+		
+		count = count + 1 + n * 4;
+		return count;
 	}
 
 }
