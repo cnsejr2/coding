df3b6d65b25795c6ad26498282921bd94a882c5a
cnsejr2 <cnsejr345@gmail.com>
2023-05-22 23:37:19 +0900

    23.05.22 - p9095

---
+++ CoingTest/src/p9095.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class p9095 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int[] dp = new int[11];
+		
+		int n = sc.nextInt();
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		for (int i = 4; i <= 10; i++) {
+			dp[i] = dp[i - 3] + dp[i - 2] +dp[i - 1];
+		}
+		
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			
+			System.out.println(dp[m]);
+		}
+		sc.close();
+
+	}
+
+}
