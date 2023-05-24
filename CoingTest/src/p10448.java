17cadf53c4ff5828e818c73b5102d30dff04d734
cnsejr2 <cnsejr345@gmail.com>
2023-05-24 23:17:56 +0900

    23.05.24 - p10448

---
+++ CoingTest/src/p10448.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class p10448 {
+	static int[] t = new int[46];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int k = sc.nextInt();
+		
+		for (int i = 1; i < 45; i++) {
+			t[i] = i * (i + 1) / 2;
+		}
+		
+		for (int i = 0; i < k; i++) {
+			int n = sc.nextInt();
+			
+			System.out.println(calculate(n));
+		}
+		
+		sc.close();
+	}
+	
+	public static int calculate(int n) {
+		
+		for (int i = 1; i < 45; i++) {
+			for (int j = 1; j < 45; j++) {
+				for (int k = 1; k < 45; k++) {
+					if (n == (t[i] + t[j] + t[k])) {
+						return 1;
+					}
+				}
+			}
+		}
+		
+		return 0;
+	}
+
+}
