a506750e862d2c2fe370ffbca7fc11180a33d646
cnsejr2 <cnsejr345@gmail.com>
2023-05-31 21:10:41 +0900

    23.05.31 - p1912

---
+++ CoingTest/src/p1912.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class p1912 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+	
+		int max = arr[0];
+		dp[0] = arr[0];
+		
+		for (int i = 1; i < n; i++) {
+			dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+			
+			max = Math.max(max,  dp[i]);
+		}
+		
+		System.out.println(max);
+		
+		sc.close();
+	}
+
+}
